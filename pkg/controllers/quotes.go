6db17631c04296ac6127325d4bc0d451356157a1
rnt <kvrenat@gmail.com>
2021-09-23 12:48:16 +0600

    add momentum alert

--- pkg/controllers/quotes.go
+++ pkg/controllers/quotes.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"strings"
 	"time"
 
 	"fx_alert/pkg/db"
@@ -12,16 +13,23 @@ import (
 )
 
 func ProcessQuotes(ctx context.Context, dbH *db.DB, qHolder *quoter.Holder, tlg *telegram.Telegram) {
-	ticker := time.NewTicker(time.Minute)
+	levelTicker := time.NewTicker(65 * time.Second)
+	defer levelTicker.Stop()
+	momentumTicker := time.NewTicker(5 * time.Minute)
+	defer momentumTicker.Stop()
 	log.Printf("Quotes controller started")
 	go qHolder.Update(ctx, 2)
 	for {
 		select {
 		case <-ctx.Done():
 			return
-		case <-ticker.C:
+		case <-levelTicker.C:
 			qHolder.Update(ctx, 2)
 			checkUsersLevelAlerts(ctx, dbH, qHolder, tlg)
+			checkMomentum(ctx, dbH, qHolder, tlg)
+		case <-momentumTicker.C:
+			qHolder.Update(ctx, 2)
+			checkMomentum(ctx, dbH, qHolder, tlg)
 		}
 	}
 }
@@ -52,7 +60,7 @@ func checkUsersLevelAlerts(ctx context.Context, dbH *db.DB, qHolder *quoter.Hold
 				continue
 			}
 			go func(ID int64, val db.Value, p float64) {
-				msg := fmt.Sprintf("Alert: %s. \n. Current: %.5f", val.String(), p)
+				msg := fmt.Sprintf("Alert: %s. \nCurrent: %.5f", val.String(), p)
 				if err := tlg.SendMessage(ID, 0, telegram.Answer{Text: msg}); err != nil {
 					log.Printf("Can't send alert: %d. %q. %v", ID, msg, err)
 					return
@@ -64,6 +72,59 @@ func checkUsersLevelAlerts(ctx context.Context, dbH *db.DB, qHolder *quoter.Hold
 				}
 				log.Printf("Deleted: %d. %q", ID, val.String())
 			}(ID, val, q.Close)
+		}
+	}
+}
+
+func checkMomentum(ctx context.Context, dbH *db.DB, qHolder *quoter.Holder, tlg *telegram.Telegram) {
+	ids := dbH.Users()
+	for _, ID := range ids {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			break
+		}
+		symbs := quoter.GetAllowedSymbols()
+		for _, symb := range symbs {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				break
+			}
+			qs, err := qHolder.GetQuote(symb)
+			if err != nil {
+				log.Printf("Can't get quotes for: %d. %q. %v", ID, symb, err)
+				continue
+			}
+			diff := qs.Current.Close - qs.Previous.Close
+			points := diff
+			if strings.EqualFold(symb, "btcusd") && (points < 500) {
+				continue
+			}
+			p := quoter.GetPrecision(symb)
+			for i := uint8(0); i < p; i++ {
+				points *= 10
+			}
+			if points < 50 {
+				continue
+			}
+			go func(ID int64, symb string, diff, points float64, qs *quoter.Quotes) {
+				msg := fmt.Sprintf(
+					"Alert: %s. \nDiff: %.0f (%.5f)\nPrevious: %.5f\nCurrent: %.5f",
+					symb,
+					points,
+					diff,
+					qs.Previous.Close,
+					qs.Current.Close,
+				)
+				if err := tlg.SendMessage(ID, 0, telegram.Answer{Text: msg}); err != nil {
+					log.Printf("Can't send alert: %d. %q. %v", ID, msg, err)
+					return
+				}
+				log.Printf("Sent alert: %d. %q", ID, msg)
+			}(ID, symb, diff, points, qs)
 		}
 	}
 }
